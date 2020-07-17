8e299c0f4b5562fae39bf5434b3405b7a7e7846d
Mike Neilens <mike.neilens@gmail.com>
2020-07-17 11:56:02 +0100

    initial version

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun `1 should be equal to 1`() {
+        assertEquals(1,1)
+    }
+}
