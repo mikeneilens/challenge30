3b6dbd717d0cf5c941155d867c1e94d490f86d70
Andrew Southcombe <andrew.southcombe@johnlewis.co.uk>
2020-07-17 15:31:13 +0100

    implements slot numbering

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,44 @@
-class Location(val id:Int,val name: String)
+class Location(val id: Int, val name: String)
 
-abstract class Space(val id: Int, val name: String,val location: Location) {
+abstract class Space(val id: Int, val name: String, val location: Location) {
     abstract fun capacity(): Int
+    abstract fun slotDurationHours(): Int
+    abstract val slots: List<Slot>
 }
 
 class Desk(id: Int, name: String, location: Location) : Space(id, name, location) {
     override fun capacity() = 1
+    override fun slotDurationHours() = 2
+    override val slots: List<Slot> = deskSlots
 }
 
 class Room(id: Int, name: String, val capacity: Int, location: Location) : Space(id, name, location) {
     override fun capacity() = capacity
+    override fun slotDurationHours() = 4
+    override val slots: List<Slot> = roomSlots
 }
 
 class DeskBank(id: Int, name: String, val desks: List<Desk>, location: Location) : Space(id, name, location) {
     override fun capacity() = desks.size
+    override fun slotDurationHours() = 2
+    override val slots: List<Slot> = deskSlots
 }
 
-typealias SlotTime = String
-
-data class Slot(val startDateTime: SlotTime, val endDateTime: SlotTime) {
-
-}
-
-data class Booking(val space: Space, val slot: Slot, val customer: String) {
+data class Slot(val slotNumber: Int, val slotDescription: String) {
 
 }
 
-interface BookingRepository{
-    fun getFreeSpacesForSlot(spaceType:String,slot:Slot,location:Location):List<Space>
-    fun getFreeSlotsForSpace(space:Space,startDateTime: SlotTime,endDateTime: SlotTime):List<Slot>
-    fun saveBooking(booking:Booking)
-    fun cancelBooking(booking:Booking)
+val deskSlots: List<Slot> = listOf(Slot(1, "0900 to 1100"), Slot(2, "1100 to 1300"))
+val roomSlots: List<Slot> = listOf(Slot(1, "0900 to 1300"), Slot(2, "1300 to 1700"))
+
+data class Booking(val space: Space, val slot: Slot, val customer: String,val bookingDate:String) {
+
+}
+
+interface BookingRepository {
+    fun getFreeSpacesForSlot(spaceType: String, slot: Slot, location: Location): List<Space>
+    fun getFreeSlotsForSpace(space: Space, startDateTime: String, endDateTime: String): List<Slot>
+    fun saveBooking(booking: Booking)
+    fun cancelBooking(booking: Booking)
 }
 
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test
 class MainTest {
     @Test
     fun `desk bank containing 2 desks returns a capacity of 2`() {
-        val desk1 = Desk(1,"desk1")
-        val desk2 = Desk(2,"desk2")
-        val deskBank = DeskBank(1,"desk2", listOf(desk1, desk2))
+        val desk1 = Desk(1,"desk1",Location(1,"Victoria"))
+        val desk2 = Desk(2,"desk2",Location(1,"Victoria"))
+        val deskBank = DeskBank(1,"desk2", listOf(desk1, desk2),Location(1,"Victoria"))
         assertEquals(2,deskBank.capacity())
     }
 }
